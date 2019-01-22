1fda471470366067726f335d29da129e92390c7e
Jacob Coriell <jtc1065@utulsa.edu>
2019-01-21 23:40:46 -0600

    editing

---
+++ bot.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    const channel = client.channels.find('name', 'general')
+    msg.reply('no u')
+    //channel.send('raven is a dodo')
+
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'jacob is a dodo') {
+    msg.reply('no u')
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'smh') {
+    msg.reply('shake so much and itll fall off')
+  }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === 'raven is a dodo') {
+    msg.reply('yup')
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'bye') {
+    msg.reply('cya')
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
