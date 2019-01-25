f96f584da85ffb91f9536db9b1bd80d503ad19b5
corielljacob <32170426+corielljacob@users.noreply.github.com>
2019-01-25 10:55:17 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -85,9 +85,9 @@ client.on('message', msg => {
   } else if (msg.content.toLowerCase().includes('nonce') || msg.content.toLowerCase().includes('n once')||msg.content.toLowerCase().includes('n0nce')){
     msg.delete(500)
   }else if (msg.content.toLowerCase().includes('food')){
-    webAttachment = new Discord.Attachment(food[getRandomInt(food.length)])
-    //const attachment = new MessageAttachment('./discordBot/images/img1.png')
-    msg.channel.send(webAttachment)
+    //webAttachment = new Discord.Attachment(food[getRandomInt(food.length)])
+    const attachment = new MessageAttachment('./discordBot/images/img1.png')
+    msg.channel.send(attachment)
   }
 });
 
