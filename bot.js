8397c51db34625feabe887b5daf1d1894d184866
corielljacob <32170426+corielljacob@users.noreply.github.com>
2019-02-04 21:20:02 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -89,6 +89,9 @@ client.on('message', msg => {
   }else if (msg.content.toLowerCase() === prefix + 'morgan') {
     webAttachment = new Discord.Attachment('https://cdn.discordapp.com/attachments/328228325862539274/539490520322539560/image0.png')
     msg.channel.send(webAttachment)
+  }else if (msg.content.toLowerCase() === prefix + 'james') {
+    webAttachment = new Discord.Attachment('https://cdn.discordapp.com/attachments/542182044898426890/542182398247567360/straight.gif')
+    msg.channel.send(webAttachment)
   }else if (msg.content.toLowerCase().includes('cunt')){
     webAttachment = new Discord.Attachment('https://cdn.discordapp.com/attachments/373285622225436682/535272121849479188/image0.jpg')
     msg.channel.send(webAttachment)
