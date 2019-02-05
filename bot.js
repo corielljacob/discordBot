eb4768fdfdabf65bdca42e49c7bcaa54f9f2f9b0
corielljacob <32170426+corielljacob@users.noreply.github.com>
2019-02-05 10:38:10 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -102,6 +102,8 @@ client.on('message', msg => {
     const num = getRandomInt(food_size);
     const attachment = new Discord.Attachment('./images/img'+num+'.png', 'img'+num+'.png');
     msg.channel.send(attachment)
+  }else if (msg.content.toLowerCase() === 'uwu'){
+    msg.delete(100)
   }
 });
 
