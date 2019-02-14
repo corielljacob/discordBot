d76026c48abbba97b8faf29e8ccf32fdf3e972f8
corielljacob <32170426+corielljacob@users.noreply.github.com>
2019-02-14 10:22:43 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -124,8 +124,9 @@ client.on('message', msg => {
       return msg.channel.send('You need to specify a correct user.')
     }
     const user = msg.mentions.users.first()
+    let userVar = message.author
     const attachment = new Discord.Attachment('./valentines/img'+num+'.jpg', 'img'+num+'.jpg');
-    msg.channel.send(`To: ${user} ` + attachment)
+    msg.channel.send(`To: ${user} From: ${userVar}`)
     msg.channel.send(attachment)
   }
 });
