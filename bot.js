04f8d318490d9c620784b6e9e9edd3695d8a2696
Unkvlt <sabrerechelle@gmail.com>
2019-06-30 23:58:09 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -91,6 +91,9 @@ client.on('message', msg => {
   }else if (msg.content.toLowerCase() === prefix + 'lode') {
     webAttachment = new Discord.Attachment('https://i.ytimg.com/vi/-GEHyAfV4OI/hqdefault.jpg')
     msg.channel.send(webAttachment)
+  }else if (msg.content.toLowerCase() === prefix + 'erin') {
+    webAttachment = new Discord.Attachment('https://media.discordapp.net/attachments/512994325743927325/594746628871684112/Screenshot_20190630-0029432.png')
+    msg.channel.send(webAttachment)
   } //else if (msg.content.toLowerCase().includes('feet')){
    //webAttachment = new Discord.Attachment('https://i.imgur.com/VWvSEqJ.jpg')
    //msg.channel.send(webAttachment)
