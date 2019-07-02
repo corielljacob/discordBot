ec82b93a5bf0c017df1c11ef41f002cb2c1944d0
corielljacob <32170426+corielljacob@users.noreply.github.com>
2019-07-02 10:06:38 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -105,6 +105,10 @@ client.on('message', msg => {
     const num = getRandomInt(food_size);
     const attachment = new Discord.Attachment('./images/img'+num+'.png', 'img'+num+'.png');
     msg.channel.send(attachment)
+  }else if(msg.content.toLowerCase().includes(prefix + 'cursedfood#') {
+    const num = parseInt(msg.content.substring(11))
+    const attachment = new Discord.Attachment('./images/img'+num+'.png', 'img'+num+'.png');
+    msg.channel.send(attachment)
   }else if (msg.content.toLowerCase().includes('uwu')){
     msg.delete(200)
 }});
