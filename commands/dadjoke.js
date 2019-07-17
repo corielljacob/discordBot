ceebc6b98d4a8e4781ba0ba00ab7eb2481ad7982
corielljacob <32170426+corielljacob@users.noreply.github.com>
2019-07-17 11:00:59 -0500

    Update dadjoke.js

--- commands/dadjoke.js
+++ commands/dadjoke.js
@@ -1,3 +1,6 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+const fetch = require('node-fetch');
 module.exports = {
 	name: 'dadjoke',
 	description: 'Sends a dad joke received from API call',
