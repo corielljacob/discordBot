3ac76d33328b42acb51491b9a53be6b739890282
corielljacob <32170426+corielljacob@users.noreply.github.com>
2019-07-17 10:57:34 -0500

    Create dadjoke.js

---
+++ commands/dadjoke.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: 'dadjoke',
+	description: 'Sends a dad joke received from API call',
+	execute(message, args) {
+    const body = await fetch('https://icanhazdadjoke.com/', {
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then(response => response.json());
+    console.log(body.joke)
+	},
+};
