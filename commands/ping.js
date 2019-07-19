a4852a51425ee6055f12ea9f818783399e8177fd
corielljacob <32170426+corielljacob@users.noreply.github.com>
2019-07-19 09:14:26 -0500

    Added utility class

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,10 @@
+const util = require(../'utility.js')
 module.exports = {
   name: 'ping',
   description: 'Ping!',
   execute(message, args) {
     message.channel.send('**Pong.**');
-    //console.log(parseInt(this.getRandomInt(6)))
+    console.log(util.getRandomInt(5))
   },
   getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
---
+++ utility.js
@@ -0,0 +1,3 @@
+getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
