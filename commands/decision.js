ef0b863d5cae23041ef6aaf56006d824e3b2cf92
corielljacob <32170426+corielljacob@users.noreply.github.com>
2019-07-29 09:32:27 -0500

    Create decision.js

---
+++ commands/decision.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+const fetch = require('node-fetch');
+const util = require('../utility.js');
+module.exports = {
+  name: 'decision',
+  description: 'Makes a yes or no decision with attached gif',
+  async execute(msg, args) {
+    const body = await fetch('http://yesno.wtf/api/').then(response => response.json());
+
+    var webAttachment = new Discord.Attachment(body.image)
+    msg.channel.send(webAttachment)
+  }
