30742f4a4ffc12fb5ea53b64eb73875aaf92a57c
corielljacob <32170426+corielljacob@users.noreply.github.com>
2019-08-09 08:51:19 -0500

    Update tom.js

--- commands/tom.js
+++ commands/tom.js
@@ -2,7 +2,7 @@ const util = require('../utility.js')
 module.exports = {
   name: 'tom',
   description: 'Do something related to thomas.',
-  execute(message, args) {
+  execute(msg, args) {
     let thom = ['Why dont ants get sick?', 'Because they have little antibodies', 'Son: What rhymes with orange?\nThomas: no it doesnt', 'Whats loud and sounds like an apple? AN APPLE', 'Whats red and smells like blue paint? red paint', 'Time flies like an arrow; fruit flies like bananas',
       'Whats red and bad for your teeth? A brick', 'I have the heart of a lion and a lifetime ban from the toronto zoo', 'i bought the worlds worst thesaurus yesterday. not only is it terrible, but its terrible',
       'to the guy who stole my MS office, i will find you. you have my word', 'my friend asked me to round up his 37 sheep. i said "40"', 'BEES?'
