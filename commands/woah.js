4bcba56b08ddc9a3c3624f7bdcbbab23240ed1b9
corielljacob <32170426+corielljacob@users.noreply.github.com>
2020-01-15 14:46:38 -0600

    Update woah.js

--- commands/woah.js
+++ commands/woah.js
@@ -6,13 +6,15 @@ module.exports = {
   name: 'woah',
   description: 'Sends Hansen?',
   async execute(msg, args) {
+    const val = 1;
     setInterval(function(){
     if(1==1)
       {
+        val = util.getRandomInt(3);
         var webAttachment = new Discord.Attachment("https://vignette.wikia.nocookie.net/to-catch-a-predator/images/2/29/6b7f722f-750a-45d8-aca4-d3db587ded7f.jpg")
         msg.client.channels.get('537142011455733770').send(webAttachment);
         msg.client.channels.get('537142011455733770').send("Why dont you take a seat...");
       }
-    }, 1000 * (60))
+    }, 1000 * (60*val))
   }
 }
