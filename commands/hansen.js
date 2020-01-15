a359cfe355de1a3358cfe0f5eb6a680006d7245a
corielljacob <32170426+corielljacob@users.noreply.github.com>
2020-01-15 14:20:38 -0600

    Update hansen.js

--- commands/hansen.js
+++ commands/hansen.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
 const config = require('../config.json');
 const fetch = require('node-fetch');
 const util = require('../utility.js');
@@ -7,6 +6,6 @@ module.exports = {
   name: 'hansen',
   description: 'Sends Hansen',
   async execute(msg, args) {
-    client.channels.get("661803881549791251").send("If this sends, hansen is coming asap")
+    msg.client.channels.get("661803881549791251").send("If this sends, hansen is coming asap")
   }
 }
