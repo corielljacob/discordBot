3603d1e2009a2e53fd1c563fc70706dd27e1da23
corielljacob <32170426+corielljacob@users.noreply.github.com>
2020-01-15 14:54:49 -0600

    Update woah.js

--- commands/woah.js
+++ commands/woah.js
@@ -10,7 +10,7 @@ module.exports = {
     setInterval(function(){
     if(1==1)
       {
-        val = util.getRandomInt(3);
+        val = util.getRandomInt(20);
         var webAttachment = new Discord.Attachment("https://vignette.wikia.nocookie.net/to-catch-a-predator/images/2/29/6b7f722f-750a-45d8-aca4-d3db587ded7f.jpg")
         msg.client.channels.get('537142011455733770').send(webAttachment);
         msg.client.channels.get('537142011455733770').send("Why dont you take a seat...");
