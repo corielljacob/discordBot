9d7a71a6d50d9fff8fefda388543da1c210f6d1c
corielljacob <32170426+corielljacob@users.noreply.github.com>
2020-01-15 17:03:36 -0600

    revamp hansen

--- commands/woah.js
+++ commands/woah.js
@@ -6,8 +6,12 @@ module.exports = {
   name: 'woah',
   description: 'Sends Hansen?',
   async execute(msg, args) {
-    while (true) {
-      util.hansen(1, msg);
-    }
+    var num = util.getRandomInt(3);
+    console.log(num);
+    if(num = 1 || num = 2) return;
+
+    var webAttachment = new Discord.Attachment("https://vignette.wikia.nocookie.net/to-catch-a-predator/images/2/29/6b7f722f-750a-45d8-aca4-d3db587ded7f.jpg")
+    msg.client.channels.get('537142011455733770').send(webAttachment);
+    msg.client.channels.get('537142011455733770').send("Why dont you take a seat...");
   }
 }
