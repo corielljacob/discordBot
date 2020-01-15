1e499253b9dca486100a7209a51d3ebe713f03cf
corielljacob <32170426+corielljacob@users.noreply.github.com>
2020-01-15 16:41:56 -0600

    added hansen to util

--- commands/woah.js
+++ commands/woah.js
@@ -6,15 +6,10 @@ module.exports = {
   name: 'woah',
   description: 'Sends Hansen?',
   async execute(msg, args) {
-    var val = 1;
-    setInterval(function(){
-    if(1==1)
-      {
-        val = util.getRandomInt(5);
-        var webAttachment = new Discord.Attachment("https://vignette.wikia.nocookie.net/to-catch-a-predator/images/2/29/6b7f722f-750a-45d8-aca4-d3db587ded7f.jpg")
-        msg.client.channels.get('537142011455733770').send(webAttachment);
-        msg.client.channels.get('537142011455733770').send("Why dont you take a seat...");
-      }
-    }, 1000 * (60*util.getRandomInt(5)))
+    util.hansen(2);
+
+
+
+
   }
 }
