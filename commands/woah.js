1add83beb1af0ef35f939e35817f2fd45e5cc022
corielljacob <32170426+corielljacob@users.noreply.github.com>
2020-01-15 14:27:29 -0600

    Create woah.js

---
+++ commands/woah.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+const fetch = require('node-fetch');
+const util = require('../utility.js');
+module.exports = {
+  name: 'woah',
+  description: 'Sends Hansen?',
+  async execute(msg, args) {
+    setInterval(function(){
+    if(currentdate.getMinutes() == currentdate.getHours())
+      {
+        msg.client.channels.get('537142011455733770').send('Touchez votre nez :nose:');
+      }
+    }, 1000)
+  }
+}
