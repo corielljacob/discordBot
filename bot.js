9fc54593746545379c160cf1c7ca6f40b4073f8c
corielljacob <32170426+corielljacob@users.noreply.github.com>
2020-01-27 19:33:55 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -73,7 +73,7 @@ client.on('message', async msg => {
     if (msg.attachments.size === 1) {
       util.setPic(msg, collection)
     } else {
-      msg.client.channels.get('671527521816150017').send(msg);
+      msg.client.channels.get('671527521816150017').send(msg.content);
     }
     return;
 
