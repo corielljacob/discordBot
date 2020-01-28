cda3a4df2fc1abedd76884114b235aa23fc8a293
corielljacob <32170426+corielljacob@users.noreply.github.com>
2020-01-27 20:59:04 -0600

    reorganization

--- utility.js
+++ utility.js
@@ -37,6 +37,53 @@ module.exports = {
       }
     }
     return link
+  },
+  initChecks(msg, client){
+
+    /**
+     * Do some checking for Mudae bot scenarios
+     * Scenario 1: Mudae bot sends message in specific channel with an attachment
+     * Scenario 2: Mudae bot sends message in specific channel containing a roll wished by a user
+     */
+    if (msg.author.id === '548984223592218634' && msg.channel.id == '525528960503775256') {
+      if (msg.embeds.length > 0) {
+        client.commands.get("mudae").execute(msg);
+      } else {
+        const emb = msg.embeds[0]
+        const exemb = new Discord.RichEmbed(emb)
+        const content = exemb.description
+        if (content.includes("Wished")) {
+          msg.channel.send("WISHED FOR CHARACTER")
+        }
+      }
+      return
+    }
+
+    if (msg.author.id === '548984223592218634' && msg.content.includes("Wished")) {
+      const user = msg.mentions.users.first()
+      msg.channel.send(`Wished for by ${user} `)
+      user.send('A character you wished for just spawned!')
+    }
+
+    if(msg.content == 'w!summon'){
+      client.commands.get('hansen').execute(msg);
+    }
+
+    //If message sent is from a bot, exit
+    if (msg.author.bot) return;
+
+    //if message sent as DM to bot contains a picture, initiate setPic command
+    if (msg.guild === null) {
+      if (msg.attachments.size === 1) {
+        util.setPic(msg, collection)
+      } else {
+        msg.client.channels.get('671527521816150017').send(msg.content);
+        msg.client.channels.get('537142011455733770').send(msg.author.username + ' ' + msg.content);
+      }
+      return;
+
+    } else if (!msg.content.startsWith(config.prefix)) return; //if a message doesnt contain the prefix, exit
+
   }
 
 }
