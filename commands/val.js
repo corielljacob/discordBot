b1cc1e75b9a7085ab53a1ec23a4c599f33161b7c
corielljacob <32170426+corielljacob@users.noreply.github.com>
2020-02-13 22:16:27 -0600

    erin and thomas

--- commands/val.js
+++ commands/val.js
@@ -9,10 +9,20 @@ module.exports = {
       msg.channel.send("Please @ a user!")
       return;
     }
-    const num = util.getRandomInt(40)
-    const attachment = new Discord.Attachment('./assets/valentines/img' + num + '.jpg', 'img' + num + '.jpg')
     const user = msg.mentions.users.first()
+    if(user.id === '206236812916555776'){
+      const attachment = new Discord.Attachment('./assets/valentines/img40.jpg', 'img' + num + '.jpg')
+      msg.channel.send(`To: ${user}\nFrom: ${msg.author}`)
+      msg.channel.send(attachment)
+    }else if(user.id === '537102010357645313'){
+      const attachment = new Discord.Attachment('./assets/valentines/img41.jpg', 'img' + num + '.jpg')
+      msg.channel.send(`To: ${user}\nFrom: ${msg.author}`)
+      msg.channel.send(attachment)
+    }else{
+    const num = util.getRandomInt(39)
+    const attachment = new Discord.Attachment('./assets/valentines/img' + num + '.jpg', 'img' + num + '.jpg')
     msg.channel.send(`To: ${user}\nFrom: ${msg.author}`)
     msg.channel.send(attachment)
+  }
   },
 }
