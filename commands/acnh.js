041fd4d6757a77d6ebea100bc1e63df61d1cb938
Jacob Coriell <coriell.jacob@gmail.com>
2020-03-23 16:46:35 -0500

    testing acnh

--- commands/acnh.js
+++ commands/acnh.js
@@ -6,7 +6,7 @@ module.exports = {
   name: 'acnh',
   description: 'acnh turnip reminder',
   async execute(msg, time) {
-    time = 0;
+    //time = 0;
     if(time === 0){
       msg.client.channels.get('537142011455733770').send(`<@&598889483995185173> It's a new day! Post your turnip prices before they change at noon relative to your timezone!`);
     } else if (time === 1){
