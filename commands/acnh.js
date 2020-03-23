283283d0f878ffd1c7c159c58a38122961203bb2
Jacob Coriell <coriell.jacob@gmail.com>
2020-03-23 16:50:19 -0500

    testing acnh

--- commands/acnh.js
+++ commands/acnh.js
@@ -6,11 +6,12 @@ module.exports = {
   name: 'acnh',
   description: 'acnh turnip reminder',
   async execute(msg, time) {
-    //time = 0;
     if(time === 0){
-      msg.client.channels.get('537142011455733770').send(`<@&598889483995185173> It's a new day! Post your turnip prices before they change at noon relative to your timezone!`);
+      msg.client.channels.get('512994325743927325').send(`<@&691633590231891979> It's a new day! Post your turnip prices before they change at noon relative to your timezone!`);
     } else if (time === 1){
-    msg.client.channels.get('537142011455733770').send(` Remember to post your post-noon turnip prices!`);
+    msg.client.channels.get('512994325743927325').send(`<@&691633590231891979> Remember to post your post-noon turnip prices!`);
+  } else if (time === null){
+    msg.client.channels.get('512994325743927325').send(`This command is used to send the daily turnip messages. It has no other functionality.`);
   }
   }
 }
