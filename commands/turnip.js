abe6e05e3fb96b8c0a8c29309d811b2820e1cf8d
corielljacob <32170426+corielljacob@users.noreply.github.com>
2020-03-28 21:10:14 -0500

    Create turnip.js

---
+++ commands/turnip.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+const fetch = require('node-fetch');
+const util = require('../utility.js');
+module.exports = {
+  name: 'turnip',
+  description: 'Calculate turnip expenditure and potential profit/loss',
+  async execute(msg) {
+    const filter1 = m1 => {
+      return m1.author.id === msg.author.id
+    }
+
+    msg.channel.awaitMessages(m1, {
+        max: 1,
+        time: 10000
+      })
+      .then(collected => {
+        const response = collected.first();
+        msg.channel.send(response)
+      });
+  });
+
+}
+}
