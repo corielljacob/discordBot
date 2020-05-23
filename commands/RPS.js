11e192aac21345023a0654c73975e9ef20a1ab13
corielljacob <32170426+corielljacob@users.noreply.github.com>
2020-05-22 20:58:59 -0500

    Update RPS.js

--- commands/RPS.js
+++ commands/RPS.js
@@ -5,6 +5,8 @@ module.exports = {
   description: 'Play Rock-Paper-Scissors',
   execute(msg, args) {
     const pepehands = msg.client.emojis.find(emoji => emoji.name === "pepehands")
+    const pepelaugh = msg.client.emojis.find(emoji => emoji.name === "pepelaugh")
+    const monkaS = msg.client.emojis.find(emoji => emoji.name === "monkaS")
     let choices = ["rock", "paper", "scissors"]
     let randomPick = util.getRandomInt(3);
     msg.channel.send("I have made my choice, now type yours to play");
@@ -18,19 +20,19 @@ module.exports = {
       let userChoice = message.content.toLowerCase();
       switch(userChoice) {
         case "rock":
-          if(randomPick == 0) msg.channel.send("We tie.")
-          else if(randomPick == 1) msg.channel.send("I win!")
+          if(randomPick == 0) msg.channel.send("We tie " + monkaS.toString())
+          else if(randomPick == 1) msg.channel.send("I win " + pepelaugh.toString())
           else msg.channel.send("I lose " + pepehands.toString())
           break
         case "paper":
-          if(randomPick == 0) msg.channel.send("I lose...")
-          else if(randomPick == 1) msg.channel.send("We tie.")
-          else msg.channel.send("I win!")
+          if(randomPick == 0) msg.channel.send("I lose " + pepehands.toString())
+          else if(randomPick == 1) msg.channel.send("We tie " + monkaS.toString())
+          else msg.channel.send("I win " + pepelaugh.toString())
           break
         case "scissors":
-          if(randomPick == 0) msg.channel.send("I win!")
-          else if(randomPick == 1) msg.channel.send("I lose...")
-          else msg.channel.send("We tie.")
+          if(randomPick == 0) msg.channel.send("I win " + pepelaugh.toString())
+          else if(randomPick == 1) msg.channel.send("I lose " + pepehands.toString())
+          else msg.channel.send("We tie " + monkaS.toString())
           break
         default:
           msg.channel.send("Thats not a valid choice. Try again.")
