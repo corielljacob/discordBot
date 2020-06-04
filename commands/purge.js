c45277eaa75bc87ab37f4d885ec58b47ead08d84
corielljacob <32170426+corielljacob@users.noreply.github.com>
2020-06-04 11:34:49 -0500

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -4,15 +4,16 @@ module.exports = {
   name: 'purge',
   description: 'Delete a specified number of messages',
   execute(msg, args) {
+
+    if(parseInt(args[0]) < 0){
+      msg.channel.send("Please enter a valid number of messages to delete.")
+      return
+    }
     try{
-      if(parseInt(args[0]) < 0){
-        msg.channel.send("Please enter a valid number of messages to delete")
-        return
-      }
       msg.channel.bulkDelete(args[0])
     }
     catch(err){
-      msg.channel.send("Please enter a valid number of messages to delete")
+      msg.channel.send("Please enter a valid number of messages to delete.")
     }
   }
 }
