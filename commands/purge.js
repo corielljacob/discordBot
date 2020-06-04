4f3abb07c855ae3363cb2d561f2c4188b97ffee9
corielljacob <32170426+corielljacob@users.noreply.github.com>
2020-06-04 11:26:32 -0500

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -5,8 +5,8 @@ module.exports = {
   description: 'Delete a specified number of messages',
   execute(msg, args) {
     msg.channel.bulkDelete(args[0])
-      .catch(console.error)
-
+      .catch(console.error) {
+        msg.channel.send("error")
+      }
   }
-
 }
