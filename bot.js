6951a415446b1c2a2e2d1f93378be45fd88239bf
corielljacob <32170426+corielljacob@users.noreply.github.com>
2020-08-03 22:22:03 -0500

    revert

--- bot.js
+++ bot.js
@@ -2,14 +2,14 @@ const Discord = require('discord.js');
 const mongo = require('mongodb').MongoClient
 const fetch = require('node-fetch');
 const fs = require('fs');
-const config = require('./config.json');
-const util = require('./utility.js');
-const dburl = process.env.dbconnection;
+const config = require('./config.json')
+const util = require('./utility.js')
+const dburl = process.env.dbconnection
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-var db, collection, command;
+var db, collection, command
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
@@ -20,42 +20,40 @@ mongo.connect(dburl, {
   useNewUrlParser: true
 }, (err, client) => {
   if (err) {
-    console.error(err);
-    return;
+    console.error(err)
+    return
   }
-  db = client.db('discordbot');
-  collection = db.collection('piclinks');
+  db = client.db('discordbot')
+  collection = db.collection('piclinks')
 })
 
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity("w!help --> commands");
+  client.user.setActivity("w!help --> commands")
 });
 
-//command handling
+//command list
 client.on('message', async msg => {
+  if(msg.author.id === '0'){
+    //msg.channel.send(`${msg.author} `)
+    return;
+  }
+  var flag = util.initChecks(msg, client, collection);
+  if (flag) return;
+  //Remove the prefix from the command and split the commands into seperate arguments
+  const args = msg.content.slice(config.prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
 
-  util.connectToServer(function(err, client) {
-    if (err) console.log(err);
-
-    var flag = util.initChecks(msg, client, collection);
-    if (flag) return;
-
-    //Remove the prefix from the command and split the commands into seperate arguments
-    const args = msg.content.slice(config.prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    //initiate dynamic command handling
-    if (config.users.includes(command)) {
-      client.commands.get('namepic').execute(msg, args, command)
-    } else {
-      try {
-        client.commands.get(command).execute(msg, args);
-      } catch (error) {
-        console.log(error)
-        msg.reply('there was an error trying to execute that command! That command may not exist, you may have entered the command incorrectly, or the bot is having issues. \nIf you are trying to set your color, the format is: w!setcolor# hexcode');
-      }
+  //initiate dynamic command handling
+  if (config.users.includes(command)) {
+    client.commands.get('namepic').execute(msg, args, command)
+  } else {
+    try {
+      client.commands.get(command).execute(msg, args);
+    } catch (error) {
+      console.log(error)
+      msg.reply('there was an error trying to execute that command! That command may not exist, you may have entered the command incorrectly, or the bot is having issues. \nIf you are trying to set your color, the format is: w!setcolor# hexcode');
     }
   });
 
