9cc50c99edd16051c6dff62e6ff19b1c29ec2f26
corielljacob <32170426+corielljacob@users.noreply.github.com>
2020-08-03 21:37:12 -0500

    add gitignore and formatting

--- bot.js
+++ bot.js
@@ -2,14 +2,14 @@ const Discord = require('discord.js');
 const mongo = require('mongodb').MongoClient
 const fetch = require('node-fetch');
 const fs = require('fs');
-const config = require('./config.json')
-const util = require('./utility.js')
-const dburl = process.env.dbconnection
+const config = require('./config.json');
+const util = require('./utility.js');
+const dburl = process.env.dbconnection;
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-var db, collection, command
+var db, collection, command;
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
@@ -20,27 +20,24 @@ mongo.connect(dburl, {
   useNewUrlParser: true
 }, (err, client) => {
   if (err) {
-    console.error(err)
-    return
+    console.error(err);
+    return;
   }
-  db = client.db('discordbot')
-  collection = db.collection('piclinks')
+  db = client.db('discordbot');
+  collection = db.collection('piclinks');
 })
 
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity("w!help --> commands")
+  client.user.setActivity("w!help --> commands");
 });
 
-//command list
+//command handling
 client.on('message', async msg => {
-  if(msg.author.id === '0'){
-    //msg.channel.send(`${msg.author} `)
-    return;
-  }
   var flag = util.initChecks(msg, client, collection);
   if (flag) return;
+
   //Remove the prefix from the command and split the commands into seperate arguments
   const args = msg.content.slice(config.prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
