add4b9c7d0df017ead82a2a0c91a686bd3b5ade9
corielljacob <32170426+corielljacob@users.noreply.github.com>
2021-05-08 15:33:02 -0500

    Adding random colorization of role

--- commands/randomcolor.js
+++ commands/randomcolor.js
@@ -7,6 +7,13 @@ module.exports = {
   description: 'Sets jacobs role to a random color',
   async execute(msg) {
     var cobRole = msg.client.guilds.cache.get('512994325307850753').roles.cache.get('512995592100904960');
-    cobRole.setColor('#AABBCC');
+    var newColor = '';
+    var myRandomNumber;
+    for(int i = 0; i < 6; i++) {
+      myRandomNumber = util.getRandomInt(16);
+      newColor += myRandomNumber.toString(16);
+    }
+    console.log('Set color to ' + newColor);
+    cobRole.setColor('#' + newColor);
   }
 }
