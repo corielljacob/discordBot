2d1a2c0b0ab775f93012da946bf0b6c857ed2c04
corielljacob <32170426+corielljacob@users.noreply.github.com>
2021-05-08 12:38:10 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -20,7 +20,7 @@ module.exports = {
 
     data.push(`**Name:** ${command.name}`);
     if (command.description) data.push(`**Description:** ${command.description}`);
-    if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+    if (command.usage) data.push(`**Usage:** ${config.prefix}${command.name} ${command.usage}`);
     msg.channel.send(data, { split: true });
 
   },
