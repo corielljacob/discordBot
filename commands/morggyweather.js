6b2cf926f2d40f18486675cd15442e0b2bea21a0
corielljacob <32170426+corielljacob@users.noreply.github.com>
2021-07-10 12:59:31 -0500

    Add more weather embed stuff

--- commands/morggyweather.js
+++ commands/morggyweather.js
@@ -10,24 +10,39 @@ module.exports = {
     //api.openweathermap.org/data/2.5/weather?q=Cypress&appid=132f7368978dfbf97c3d94d57c81e72a&units=imperial
     //var body = await fetch('https://www.googleapis.com/customsearch/v1?' + q + '&' + num + '&' + start + '&' + imgSize + '&' + searchType + '&' + key + '&' + cx)
     var body = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Cypress&appid=' + key + '&units=imperial')
-    .then(response => {
-      if (!response.ok) {
-        console.log('error with call to get morgan weather!')
-        throw response
-      }
-      return response.json()
-    })
+      .then(response => {
+        if (!response.ok) {
+          console.log('error with call to get morgan weather!')
+          throw response
+        }
+        return response.json()
+      })
 
     var weather_icon_code = body.weather[0].icon;
     //https://openweathermap.org/img/wn/04d.png
     var weather_icon_url = 'https://openweathermap.org/img/wn/' + weather_icon_code + '.png'
 
-    const apodEmbed = new Discord.MessageEmbed()
+    const weatherEmbed = new Discord.MessageEmbed()
       .setColor('#39ff14')
-      .setTitle(`Weather for Cypress CA`)
       .setAuthor(`Morgan's Weather`)
       //.setDescription(body.explanation)
+      .addFields({
+        name: 'TODAY\'s FORECAST'
+      }, {
+        name: 'Current',
+        value: 'Some value here',
+        inline: true
+      }, {
+        name: 'Today\'s Low',
+        value: 'Some value here',
+        inline: true
+      }, {
+        name: 'Today\'s High',
+        value: 'Some value here',
+        inline: true
+      }, )
       .setThumbnail(weather_icon_url)
-    msg.channel.send(apodEmbed)
+      .setFooter('Weather for Cypress, CA by OpenWeather');
+    msg.channel.send(weatherEmbed)
   },
 };
