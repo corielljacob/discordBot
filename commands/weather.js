b2f6820fc2c2aa702947efb3a42faa07cc651abe
corielljacob <32170426+corielljacob@users.noreply.github.com>
2021-07-12 20:23:59 -0500

    Add more dynamic weather functionality

--- commands/weather.js
+++ commands/weather.js
@@ -25,19 +25,64 @@ module.exports = {
       collection.find().toArray((findErr, items) => {
         items.forEach(function(element) {
           if (element.id == user.id) {
-            console.log('matched a user')
             if (element.location != 'null') {
               loc = element.location;
             }
           }
         })
-        //loc = 'null';
         this.buildEmbed(msg, user, loc);
       })
     })
   },
   buildEmbed(msg, user, loc) {
-    console.log(`User ${user.id} has loc ${loc}`)
-    msg.channel.send(loc)
+    if(loc == 'null' || loc == null) {
+      msg.channel.send('User either not registered in database or does not have a location stored')
+    } else {
+      var key = process.env.weather_key
+      var body = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + loc +'&appid=' + key + '&units=imperial')
+        .then(response => {
+          if (!response.ok) {
+            console.log(`error with call to get weather for ${user.id}!`)
+            throw response
+          }
+          return response.json()
+        })
+
+      var weather_icon_code = body.weather[0].icon;
+      var current_temp = body.main.temp + '°F';
+      var low = body.main.temp_min + '°F';
+      var high = body.main.temp_max + '°F';
+      var humidity = body.main.humidity + '%';
+      var wind_speed = body.wind.speed + ' MPH'
+
+      const weatherEmbed = new Discord.MessageEmbed()
+        .setColor(msg.member.displayHexColor)
+        .setAuthor(`${user.username}'s Weather`)
+        .addFields({
+          name: 'Current',
+          value: current_temp,
+          inline: true
+        }, {
+          name: 'Today\'s Low',
+          value: low,
+          inline: true
+        }, {
+          name: 'Today\'s High',
+          value: high,
+          inline: true
+        }, )
+        .addFields({
+          name: 'Humidity',
+          value: humidity,
+          inline: true
+        }, {
+          name: 'Avg Wind Speed',
+          value: wind_speed,
+          inline: true
+        }, )
+        .setThumbnail(weather_icon_url)
+        .setFooter(`Weather for ${loc} by OpenWeather`);
+      msg.channel.send(weatherEmbed)
+    }
   },
 };
